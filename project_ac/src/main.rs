138e8c5c32ada13c151b2622a0c5a029cc53dd09
Alisson Asunción Constantine Macías <as.maciasm@gmail.com>
2023-08-09 16:03:20 -0500

    Update main.rs

--- project_ac/src/main.rs
+++ project_ac/src/main.rs
@@ -9,8 +9,6 @@ fn main () {
     let mut x = 10; //mut (mutable) para que se pueda moficiar luego // let forma de definir la funcion
     println!("Hello, world!");
     println!("x is {}", x);
-    x=20;
-    println!("x is {}", x);
 }
 
 
